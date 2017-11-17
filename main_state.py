e2a1d3d2250d44d2ad59217350a850054353fd8d
rikkay <hoolim2@gmail.com>
2017-11-17 17:56:10 +0900

    1117 projects
    attack effect

--- main_state.py
+++ main_state.py
@@ -77,10 +77,11 @@ class Background:
 
 
 class lineOne:
-
     def __init__(self):
         self.x, self.y = 640-charSize1/2,100
         self.dir=1
+        self.flyspd = 500*random.randrange(1, 5)
+        self.delayTime = 0
         self.image = load_image('hero_sprite.png')
 
     def update(self,frame_time):
@@ -92,27 +93,32 @@ class lineOne:
             elif self.x <= (640-charSize1/2)-5:
                 self.dir = 10*frame_time
         elif isPlaying==True:
-            print('call')
-            if right_key_down == True:
-                self.x += 2000*frame_time
-            elif left_key_down == True:
-                self.x -= 2000*frame_time
-            elif up_key_down == True:
-                self.y += 2000*frame_time
-            if self.x > 1280 or self.x < 0 or self.y > 720:
+            self.delayTime+=frame_time
+            if up_key_down == True and self.delayTime>0.4:
+                self.y += 1500 * frame_time
+            if self.delayTime > 0.7:
+                if right_key_down == True:
+                    self.x += 1500*frame_time
+                    self.y += self.flyspd*frame_time
+                    self.flyspd -= 5000*frame_time
+                elif left_key_down == True:
+                    self.x -= 1500*frame_time
+                    self.y += self.flyspd * frame_time
+                    self.flyspd -= 5000 * frame_time
+            if self.x > 1280 or self.x < -charSize1 or self.y > 720:
                 isPlaying = False
                 right_key_down = False
                 left_key_down = False
                 up_key_down = False
                 self.x, self.y = 640 - charSize1 / 2, 100
+                self.flyspd=500*random.randrange(3, 6)
+                self.delayTime=0
                 charNumX_one = -1
                 charNumY_one = -1
 
     def draw(self):
         self.image.clip_draw_to_origin(charNumX_one*charsize,charNumY_one*charsize,charsize,charsize,self.x,self.y,charSize1,charSize1)
         self.image.clip_draw
-
-
 
 
 class lineTwo:
@@ -168,7 +174,6 @@ class lineFour:
     def draw(self):
         self.image.clip_draw_to_origin(charNumX_four*charsize,charNumY_four*charsize,charsize,charsize,self.x,self.y,charSize4,charSize4)
 
-
 class lineFive:
     global changeSign
     def __init__(self):
@@ -187,27 +192,170 @@ class lineFive:
     def draw(self):
         self.image.clip_draw_to_origin(charNumX_five*charsize,charNumY_five*charsize,charsize,charsize,self.x,self.y,charSize5,charSize5)
 
+class slashEffect:
+
+    def __init__(self):
+        self.Yframes = 4
+        self.play_frames = 0
+        self.framepass=0
+        self.image = load_image('fx_slash.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 4
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames * 192, self.Yframes * 192, 192, 192, 390, -50, 500, 500)
+
+class slashadEffect:
+
+    def __init__(self):
+        self.Yframes = 6
+        self.play_frames = 0
+        self.framepass=0
+        self.image = load_image('fx_slashadvence.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 6
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames * 192, self.Yframes * 192, 192, 192, 390, -50, 500, 500)
+
+class magicEffect:
+
+    def __init__(self):
+        self.Yframes = 5
+        self.play_frames = 0
+        self.framepass=0
+        self.image = load_image('fx_magic.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 5
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames*192, self.Yframes*192,192,192,390,-50,500,500)
+
+class drainEffect:
+
+    def __init__(self):
+        self.Yframes = 5
+        self.play_frames = 0
+        self.framepass = 0
+        self.image = load_image('fx_darkness.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 5
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames*192, self.Yframes*192,192,192,390,-50,500,500)
+
+class healEffect:
+
+    def __init__(self):
+        self.Yframes = 4
+        self.play_frames = 0
+        self.framepass =0
+        self.image = load_image('fx_heal.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 4
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames*192, self.Yframes*192,192,192,390,-50,500,500)
+
+class bloodEffect:
+
+    def __init__(self):
+        self.Yframes = 10
+        self.play_frames = 0
+        self.framepass =0
+        self.image = load_image('fx_blood.png')
+
+    def update(self):
+        if isPlaying==True:
+            if self.framepass>=2:
+                self.play_frames = (self.play_frames + 1) % 5
+                self.framepass=0
+                if self.play_frames==0:
+                    self.Yframes -=1
+            self.framepass += 1
+        if isPlaying==False:
+            self.Yframes = 10
+
+    def draw(self):
+        self.image.clip_draw_to_origin(self.play_frames*192, self.Yframes*192,192,192,390,-50,500,500)
+
 def enter():
-    global background,line_one,line_two,line_three,line_four,line_five
+    global background,line_one,line_two,line_three,line_four,line_five,slash_fx,magic_fx,heal_fx,drain_fx,slashad_fx,blood_fx
     createCharacter()
     line_one=lineOne()
     line_two = lineTwo()
     line_three = lineThree()
     line_four = lineFour()
     line_five = lineFive()
+    slash_fx = slashEffect()
+    slashad_fx = slashadEffect()
+    magic_fx = magicEffect()
+    heal_fx= healEffect()
+    drain_fx= drainEffect()
+    blood_fx = bloodEffect()
 
     background = Background()
     pass
 
 
 def exit():
-    global background,line_one,line_two,line_three,line_four,line_five
+    global background,line_one,line_two,line_three,line_four,line_five,slash_fx,magic_fx,heal_fx,drain_fx,slashad_fx,blood_fx
     del(background)
     del(line_one)
     del(line_two)
     del(line_three)
     del(line_four)
     del (line_five)
+    del (slash_fx)
+    del (magic_fx)
+    del (heal_fx)
+    del(drain_fx)
+    del (blood_fx)
     del(pause)
     pass
 
@@ -240,15 +388,16 @@ def handle_events():
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_SPACE:
             interrogation()
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
-            right_key_down = True
-            isPlaying=True
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
-            left_key_down = True
-            isPlaying = True
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
-            up_key_down=True
-            isPlaying = True
+        if isPlaying == False:
+            if event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
+                right_key_down = True
+                isPlaying=True
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_LEFT:
+                left_key_down = True
+                isPlaying = True
+            elif event.type == SDL_KEYDOWN and event.key == SDLK_UP:
+                up_key_down=True
+                isPlaying = True
     pass
 
 def update():
@@ -259,6 +408,12 @@ def update():
     line_three.update(frame_time)
     line_four.update(frame_time)
     line_five.update(frame_time)
+    slash_fx.update()
+    slashad_fx.update()
+    magic_fx.update()
+    heal_fx.update()
+    drain_fx.update()
+    blood_fx.update()
 
     pass
 
@@ -272,8 +427,27 @@ def draw():
     line_three.draw()
     line_two.draw()
     line_one.draw()
+    #-------------------
+    if left_key_down == True:
+        if isPlaying == True:
+            blood_fx.draw()
+            slash_fx.draw()
+            slashad_fx.draw()
+
+    if right_key_down == True:
+        if isPlaying==True:
+            blood_fx.draw()
+            drain_fx.draw()
+            magic_fx.draw()
+
+
+    if up_key_down == True:
+        if isPlaying == True:
+            heal_fx.draw()
     #-----------------
     update_canvas()
+
+    delay(0.01)
     pass
 
 def changeLine():
